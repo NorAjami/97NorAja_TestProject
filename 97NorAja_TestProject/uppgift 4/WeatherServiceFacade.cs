e550f7421768748cf5aeff32b697ffde34453edb
Nor Ajami <norajami@gmail.com>
2025-01-13 09:15:38 +0100

    skapat mappar

---
+++ 97NorAja_TestProject/uppgift 4/WeatherServiceFacade.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _97NorAja_TestProject.uppgift_4
+{
+    internal class WeatherServiceFacade
+    {
+    }
+}
