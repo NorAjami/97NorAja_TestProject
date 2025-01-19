f9309509528bc111eedb57e34fbc973663954d13
Nor Ajami <norajami@gmail.com>
2025-01-19 12:47:15 +0100

    upg  8 klar

---
+++ 97NorAja_TestProject/uppgift 8/ObjectValidator.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+//using.Reflection;
+using System.Reflection;
+
+namespace _97NorAja_TestProject.uppgift_8
+{
+    public class ObjectValidator
+    {
+
+        // Metod som kontrollerar om ett objekt är null
+        public bool IsNull(object obj)
+        {
+            return obj == null;
+        }
+
+        // Metod som returnerar en lista av null-properties i ett objekt
+        public List<string> GetNullProperties(object obj)
+        {
+            if (obj == null) throw new ArgumentNullException(nameof(obj), "Object cannot be null.");
+
+            var nullProperties = new List<string>();
+
+            // Hämta alla properties för objektets typ
+            var properties = obj.GetType().GetProperties(BindingFlags.Public | BindingFlags.Instance);
+
+            foreach (var property in properties)
+            {
+                // Kontrollera om propertyn är null
+                var value = property.GetValue(obj);
+                if (value == null)
+                {
+                    nullProperties.Add(property.Name);
+                }
+            }
+
+            return nullProperties;
+        }
+
+    }
+}
---
+++ 97NorAja_TestProject/uppgift 8/ObjectValidatorTests.cs
@@ -0,0 +1,87 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+
+namespace _97NorAja_TestProject.uppgift_8
+{
+    [TestClass]
+    public class ObjectValidatorTests
+    {
+        [TestMethod]
+        public void IsNull_ShouldReturnTrue_WhenObjectIsNull()
+        {
+            // ARRANGE
+            var validator = new ObjectValidator();
+
+            // ACT
+            var result = validator.IsNull(null);
+
+            // ASSERT
+            Assert.IsTrue(result);
+        }
+
+        [TestMethod]
+        public void IsNull_ShouldReturnFalse_WhenObjectIsNotNull()
+        {
+            // ARRANGE
+            var validator = new ObjectValidator();
+
+            // ACT
+            var result = validator.IsNull(new object());
+
+            // ASSERT
+            Assert.IsFalse(result);
+        }
+
+        [TestMethod]
+        public void GetNullProperties_ShouldReturnListOfNullProperties()
+        {
+            // ARRANGE
+            var validator = new ObjectValidator();
+            var testObject = new
+            {
+                Name = "Test",
+                Age = (int?)null,
+                Address = (string)null
+            };
+
+            // ACT
+            var result = validator.GetNullProperties(testObject);
+
+            // ASSERT
+            CollectionAssert.AreEqual(new List<string> { "Age", "Address" }, result);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void GetNullProperties_ShouldThrowArgumentNullException_WhenObjectIsNull()
+        {
+            // ARRANGE
+            var validator = new ObjectValidator();
+
+            // ACT
+            validator.GetNullProperties(null);
+
+            // ASSERT: Hanteras av ExpectedException
+        }
+
+        [TestMethod]
+        public void GetNullProperties_ShouldReturnEmptyList_WhenNoPropertiesAreNull()
+        {
+            // ARRANGE
+            var validator = new ObjectValidator();
+            var testObject = new
+            {
+                Name = "Nor",
+                Age = 27,
+                Address = "123 Street"
+            };
+
+            // ACT
+            var result = validator.GetNullProperties(testObject);
+
+            // ASSERT
+            Assert.AreEqual(0, result.Count);
+        }
+    }
+}
